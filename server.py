2856879cb2979ea727c92487d35bce2bc388caba
Thomas Dalla Piazza <thomas.dallapiazza@gmail.com>
2025-03-18 15:48:25 +0100

    Basic function ok

--- server.py
+++ server.py
@@ -1,23 +1,125 @@
 
+from moonraker_api import MoonrakerListener, MoonrakerClient
+from moonraker_api.const import (
+    WEBSOCKET_STATE_CONNECTING,
+    WEBSOCKET_STATE_CONNECTED,
+    WEBSOCKET_STATE_STOPPING,
+    WEBSOCKET_STATE_STOPPED
+)
+from moonraker_api.websockets.websocketclient import (
+    ClientNotAuthenticatedError,
+)
 import logging
+import asyncio
+from asyncua import Server, ua
+from asyncua.common.methods import uamethod
 
-from opcuaserver import Opcua_server as server
-from moonrakerclient import APIConnector as client
 
-class Server:
-    def __init__(self):
-        self._logger = logging.getLogger(__name__)
-        # setup our server
-        self.server = server()
-        self.client = client()
+HOST = "localhost"
+PORT = 7125
 
-    def start(self):
-        self.server.start()
-        self.client.start()
+logging.basicConfig(
+    level=logging.WARNING, format="%(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("moonraker_api").setLevel(logging.DEBUG)
+logging.getLogger(__name__).setLevel(logging.DEBUG)
+_LOGGER = logging.getLogger(__name__)
 
+@uamethod
+def func(parent, value):
+    return value * 2
+
+async def increment_coro(value):
+    while True:
+        await asyncio.sleep(1)
+        value+=0.1
+
+class OpcuaConnector(MoonrakerListener):
+    def __init__(self, endpoint, uri):
+        self.running = False
+        self.client = MoonrakerClient(
+            self,
+            HOST,
+            PORT,
+        )
+        self._logger = _LOGGER
+        # setup our opc server
+        self.server = Server()
+        self.endpoint=endpoint
+        self.uri=uri
+
+        # append the opcua server to the event loop
+
+
+    async def setup_addresse_space(self, endpoint, uri):
+        await self.server.init()
+        self.server.set_endpoint(endpoint)
+
+        # set up our own namespace, not really necessary but should as spec
+        self.idx = await self.server.register_namespace(uri)
+        # populating our address space
+        # server.nodes, contains links to very common nodes like objects and root
+        myobj = await self.server.nodes.objects.add_object(self.idx, "MyObject")
+        self.myvar = await myobj.add_variable(self.idx, "MyVariable", 6.7)
+        # Set MyVariable to be writable by clients
+        await self.myvar.set_writable()
+        await self.server.nodes.objects.add_method(
+            ua.NodeId("ServerMethod", self.idx),
+            ua.QualifiedName("ServerMethod", self.idx),
+            func,
+            [ua.VariantType.Int64],
+            [ua.VariantType.Int64],
+        )
+
+    async def start(self) -> None:
+        """Start the websocket connection."""
+        self.running = True
+        return await self.client.connect()
+
+    async def stop(self) -> None:
+        """Stop the websocket connection."""
+        self.running = False
+        await self.client.disconnect()
+    
+
+    async def state_changed(self, state: str) -> None:
+        """Notifies of changing websocket state."""
+        self._logger.debug("Stated changed to %s", state)
+        if state == WEBSOCKET_STATE_CONNECTING:
+            pass
+        elif state == WEBSOCKET_STATE_CONNECTED:
+            pass
+        elif state == WEBSOCKET_STATE_STOPPING:
+            pass
+        elif state == WEBSOCKET_STATE_STOPPED:
+            pass
+
+    async def on_exception(self, exception: BaseException) -> None:
+        """Notifies of exceptions from the websocket run loop."""
+        self._logger.debug("Received exception from API websocket %s", str(exception))
+        if isinstance(exception, ClientNotAuthenticatedError):
+            self.entry.async_start_reauth(self.hass)
+        else:
+            raise exception
+
+    async def on_notification(self, method: str, data: any) -> None:
+        """Notifies of state updates."""
+        self._logger.debug("Received notification %s -> %s", method, data)
+
+        # Subscription notifications
+        if method == "notify_status_update":
+            message = data[0]
+            timestamp = data[1]
+            self._logger.debug("Received status update notnificatio %s -> %s", timestamp, message)
+
+async def main():
+    listener = OpcuaConnector("opc.tcp://0.0.0.0:4840/freeopcua/server/", "http://automation.ceff.ch")
+    client = listener.client
+    await client.connect()
+    await client.call_method("printer.info")
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
 
-    srv = Server()
-    srv.start()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
